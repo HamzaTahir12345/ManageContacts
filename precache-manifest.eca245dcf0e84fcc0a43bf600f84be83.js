7a2811748891dc522401395bcc5531388447b012
unknown <HamzaTahir12345>
2019-09-22 17:03:15 +0500

    Updates

---
+++ precache-manifest.eca245dcf0e84fcc0a43bf600f84be83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbc96f706edcf36889001536cd306bc6",
+    "url": "/ManageContacts/index.html"
+  },
+  {
+    "revision": "9ed4ed065a2aca9a25c4",
+    "url": "/ManageContacts/static/css/2.d7a94725.chunk.css"
+  },
+  {
+    "revision": "fc6d07df00a26e56564e",
+    "url": "/ManageContacts/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "9ed4ed065a2aca9a25c4",
+    "url": "/ManageContacts/static/js/2.8362912d.chunk.js"
+  },
+  {
+    "revision": "fc6d07df00a26e56564e",
+    "url": "/ManageContacts/static/js/main.a9695c25.chunk.js"
+  },
+  {
+    "revision": "d6bf511b0268aab8451f",
+    "url": "/ManageContacts/static/js/runtime~main.21ad62d5.js"
+  }
+]);
